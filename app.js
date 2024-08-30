8167fccc49dab6c1f294374a8815ce3944428e64
Jen <jen@example.com>
2024-08-30 09:55:30 +0100

    add initial test to get all users and refactored some of last section

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const { getAllTopics, getAllEndPoints, getArticlesById, getAllArticles, getCommentsByArticleId, postNewComment, updateArticleById, deleteCommentById } = require('./controllers/topics.controllers')
+const { getAllTopics, getAllEndPoints, getArticlesById, getAllArticles, getCommentsByArticleId, postNewComment, updateArticleById, deleteCommentById, getAllUsers } = require('./controllers/topics.controllers')
 app.use(express.json());
 
 app.get('/api/topics', getAllTopics);
@@ -18,5 +18,7 @@ app.post('/api/articles/:article_id/comments', postNewComment);
 app.patch('/api/articles/:article_id', updateArticleById);
 
 app.delete('/api/comments/:comment_id', deleteCommentById)
+
+app.get('/api/users', getAllUsers)
 
 module.exports = app;
