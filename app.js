e3c90cfb6f2ae276c1e966c306b442d1d93ae695
Jen <jen@example.com>
2024-09-16 10:30:51 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+app.use(cors());
 const app = express();
 const { getAllTopics, getAllEndPoints, getArticlesById, getAllArticles, getCommentsByArticleId, postNewComment, updateArticleById, deleteCommentById, getAllUsers } = require('./controllers/topics.controllers')
 app.use(express.json());
